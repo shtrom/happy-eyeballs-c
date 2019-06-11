6ab057a70e067a0dbcee85892fda9541cc917cc6
Olivier Mehani <shtrom@ssji.net>
2019-06-11 20:49:33 +1000

    Add timing
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#include <sys/timeb.h>
+
 struct app_config {
 	char *host;
 	char *service;
@@ -14,12 +16,14 @@ struct app_config {
 
 int parse_argv(struct app_config *conf, int argc, char ** argv);
 int connect_gai(char *host, char *service);
+void print_delta(struct timeb *start, struct timeb *stop);
 int try_read(int sfd);
 
 int main(int argc, char **argv) {
 	struct app_config conf;
 	int ret = 0;
 	int sfd = 0;
+	struct timeb start, stop;
 
 	if (0 != (ret = parse_argv(&conf, argc, argv))) {
 		fprintf(stderr, "error: parsing arguments: %d\n", ret);
@@ -29,18 +33,26 @@ int main(int argc, char **argv) {
 
 	fprintf(stderr, "happy-eyeballing %s:%s ... \n", conf.host, conf.service);
 
+	ftime(&start);
 	if((sfd = connect_gai(conf.host, conf.service)) < 0)
 	{
 		fprintf(stderr, "error: connecting: %d\n", sfd);
 		return sfd;
 	}
+	ftime(&stop);
+
+	print_delta(&start, &stop);
 
 	fprintf(stderr, "reading ...\n");
 
+	ftime(&start);
 	if ((ret = try_read(sfd)) < 0) {
 		fprintf(stderr, "error: reading: %d\n", ret);
 		return ret;
 	}
+	ftime(&stop);
+
+	print_delta(&start, &stop);
 
 	return ret;
 }
@@ -103,6 +115,13 @@ int connect_gai(char *host, char *service) {
 
 	return sfd;
 }
+
+void print_delta(struct timeb *start, struct timeb *stop) {
+	fprintf(stderr, "delta: %lds %dms\n",
+			stop->time - start->time,
+			stop->millitm-start->millitm);
+}
+
 
 int try_read(int sfd) {
 	char buf[1024];
