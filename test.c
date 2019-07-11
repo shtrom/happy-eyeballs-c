54048082ba07fb4b49747317249d2a7182865aa3
Olivier Mehani <shtrom@ssji.net>
2019-07-11 21:19:45 +1000

    fixup! Add rudimentary test

--- test.c
+++ test.c
@@ -13,6 +13,9 @@ void test_context() {
 
 	ctx = rfc6555_context_create();
 	assert(ctx != NULL);
+	assert(ctx->fds != NULL);
+	assert(ctx->original_flags != NULL);
+	assert(ctx->rps != NULL);
 
 	rfc6555_context_destroy(ctx);
 }
